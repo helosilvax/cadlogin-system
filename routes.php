43728e538d98988d1bd43739eaffaa0cedd9c395
helosilvax <heloiseribeirodasilva2006@gmail.com>
2024-09-11 08:58:12 -0300

    Adição de controladores

---
+++ routes.php
@@ -0,0 +1,8 @@
+<?php
+
+// Inclui arquivos de controlador nescessários para lidar com diferentes ações 
+require 'controllers/AuthController.php'; // Inclui o controlador de autenticação
+require 'controllers/UserController.php'; // Inclui o controlador de usuários
+require 'controllers/DashboardController'; // Inclui o controlador de dashboard
+
+?>
