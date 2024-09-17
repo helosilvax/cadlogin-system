6b27ac205d85653b5e19c9b8d6b428b7c0f6ad0d
helosilvax <heloiseribeirodasilva2006@gmail.com>
2024-09-17 08:28:12 -0300

    Cria Controlador de Usuário com as seguintes funções:  - Função de registro (register) que coleta  as informações do formulário de registro usuário e as  armazena em um array.

---
+++ controllers/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+// Requer o arquivo user que contém o model user com as funções de manipulação de dados de usuários
+require_once 'models/user.php';
+
+class AuthController
+{
+    // Função responsável processo de login 
+    public function login()
+    {
+        // Verifica se a requisição HTTP é do topo POST, ou seja, se o formulário foi enviado 
+    }
+}
+
+
+?>
